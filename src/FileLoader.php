6a855c98948d2790207688274184c5a883b0013f
Michael Wilson <chrismichaels84@users.noreply.github.com>
2016-03-11 15:03:46 -0600

    adds multiple manager classes and updates interfaces

--- src/FileLoader.php
+++ src/FileLoader.php
@@ -137,7 +137,7 @@ class FileLoader
 
     /**
      * Check to make sure the mime type is ok.
-     * @param $type a mime type
+     * @param string $type A mime type
      * @return bool
      */
     protected function isSupportedMimeType($type)
@@ -188,7 +188,7 @@ class FileLoader
 
     /**
      * Decodes a single file using registered decoders
-     * @param $file
+     * @param \SplFileInfo $file
      * @return null
      */
     protected function decodeFile($file)
--- src/UberManager.php
+++ src/UberManager.php
@@ -7,29 +7,41 @@ use Interop\Container\ContainerInterface;
 use IteratorAggregate;
 use JsonSerializable;
 use Michaels\Manager\Contracts\ChainsNestedItemsInterface;
+use Michaels\Manager\Contracts\IocContainerInterface;
+use Michaels\Manager\Contracts\IocManagerInterface;
+use Michaels\Manager\Contracts\LoadsFilesInterface;
 use Michaels\Manager\Contracts\ManagesItemsInterface;
 use Michaels\Manager\Traits\ArrayableTrait;
 use Michaels\Manager\Traits\ChainsNestedItemsTrait;
+use Michaels\Manager\Traits\LoadsFilesTrait;
+use Michaels\Manager\Traits\ManagesIocTrait;
 use Michaels\Manager\Traits\ManagesItemsTrait;
 
 /**
  * Manages deeply nested, complex data.
  *
- * This concrete class implements ManagesItems and ChainsNestedItems as well as
- * Container interoperability and various array functionality.
+ * This manager class does it all, and is more a proof of concept than anything else.
+ * It manages dependencies, loads files, uses magic methods, is arrayable, and all the rest.
  *
  * @package Michaels\Manager
  */
-class Manager implements
+class UberManager implements
     ManagesItemsInterface,
     ChainsNestedItemsInterface,
-    ContainerInterface,
+    IocManagerInterface,
+    LoadsFilesInterface,
+
+    // Array Access
     ArrayAccess,
     Countable,
     IteratorAggregate,
-    JsonSerializable
+    JsonSerializable,
+
+    // Standards
+    ContainerInterface
+
 {
-    use ManagesItemsTrait, ChainsNestedItemsTrait, ArrayableTrait;
+    use ManagesItemsTrait, ChainsNestedItemsTrait, ArrayableTrait, ManagesIocTrait, LoadsFilesTrait;
 
     /**
      * The items stored in the manager
